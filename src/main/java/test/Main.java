3c9c477efe5764a8c2fd93a17dad9b301dbd2d6f
Denis Tyshchenko <denis.tyschenko@gmail.com>
2017-02-22 18:21:31 +0100

    Adding technical files stuff to repo

---
+++ src/main/java/test/Main.java
@@ -0,0 +1,11 @@
+package test;
+
+/**
+ * Created by denis on 2/22/17.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
