3ea6853dcc41f8d0b2ac97fb6bfa4d2ae7ae358c
Denis Tyshchenko <denis.tyschenko@gmail.com>
2017-02-22 21:29:54 +0100

    Added possible patterns and returning valid regions

--- src/main/java/com/github/andone/launcher/Pattern.java
+++ src/main/java/com/github/andone/launcher/Pattern.java
@@ -1,5 +1,6 @@
 package com.github.andone.launcher;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -11,6 +12,20 @@ public class Pattern {
     private final int y;
 
     public static List<Pattern> getPatterns() {
+        return Arrays.asList(
+                new Pattern(5 ,0),
+                new Pattern(0 ,5),
+                new Pattern(0 ,4),
+                new Pattern(4 ,0),
+                new Pattern(2 ,2),
+                new Pattern(0 ,3),
+                new Pattern(3 ,0),
+                new Pattern(2 ,0),
+                new Pattern(0 ,2)
+        );
+    }
+
+    public static List<List<Cell>> getValidRegions() {
         return Collections.emptyList();
     }
 
