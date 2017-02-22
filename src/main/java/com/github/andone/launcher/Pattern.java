185885a384fc913841d8583c5247883cbdbe239f
Denis Tyshchenko <denis.tyschenko@gmail.com>
2017-02-22 21:59:53 +0100

    Return valid regions bu target cell after all possible patterns application

--- src/main/java/com/github/andone/launcher/Pattern.java
+++ src/main/java/com/github/andone/launcher/Pattern.java
@@ -1,7 +1,7 @@
 package com.github.andone.launcher;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -13,21 +13,49 @@ public class Pattern {
 
     public static List<Pattern> getPatterns() {
         return Arrays.asList(
-                new Pattern(5 ,0),
-                new Pattern(0 ,5),
-                new Pattern(0 ,4),
-                new Pattern(4 ,0),
-                new Pattern(2 ,2),
-                new Pattern(0 ,3),
-                new Pattern(3 ,0),
-                new Pattern(2 ,0),
-                new Pattern(0 ,2)
+                new Pattern(4, 0),
+                new Pattern(0, 4),
+                new Pattern(0, 3),
+                new Pattern(3, 0),
+                new Pattern(1, 1),
+                new Pattern(0, 2),
+                new Pattern(2, 0),
+                new Pattern(1, 0),
+                new Pattern(0, 1)
         );
     }
 
-    public static List<List<Cell>> getValidRegions(int x, int y) {
+    public static List<List<Cell>> getValidRegions(int x, int y, Pizza pizza) {
+        List<List<Cell>> regions = new ArrayList<>();
+        for (Pattern pattern : getPatterns()) {
+            int dx = pattern.x;
+            int dy = pattern.y;
 
-        return Collections.emptyList();
+            for (int i = -1; i <= 1; i += 2) {
+                for (int j = -1; j <= 1; j += 2) {
+                    int x2 = x + dx * i;
+                    int y2 = y + dy * j;
+                    if (isValidRegion(x + x2, y + y2, pizza)) {
+                        regions.add(cellsInRegion(x, y, x2, y2, pizza));
+                    }
+                }
+            }
+        }
+        return regions;
+    }
+
+    private static boolean isValidRegion(int x, int y, Pizza pizza) {
+        return x < pizza.getRows() && y < pizza.getCols() && x >= 0 && y >= 0;
+    }
+
+    private static List<Cell> cellsInRegion(int x0, int y0, int dx, int dy, Pizza pizza) {
+        List<Cell> region = new ArrayList<>();
+        for (int i = x0; i <= dx; i++) {
+            for (int j = y0; j <= dy; j++) {
+                region.add(pizza.get(i, j));
+            }
+        }
+        return region;
     }
 
     public Pattern(int x, int y) {
